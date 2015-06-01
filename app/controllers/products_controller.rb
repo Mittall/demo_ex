77be4153af125e381e2270bd33b568f1e6049266
mittall <mittalp.virtueinfo@gmail.com>
2015-06-01 12:50:18 +0530

    add method to application controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   #
-  autocomplete :category, :name
+  #autocomplete :category, :name
 
   # GET /products
   # GET /products.json
